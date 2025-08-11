5c2cef536673498681c6fc4c9d5e4f69317acf7b
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-08-11 09:57:25 +0530

    01_variable.js updated

--- 01_variable.js
+++ 01_variable.js
@@ -1,7 +1,6 @@
 let a=5;//variable declared using let cannot be redeclared.
 a=6;//variable declared using let can be redefine.
 const b=6;//variable declared using const cannot be redefine.
-b=8;
-
+b=9;
 
 
